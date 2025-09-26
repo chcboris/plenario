46e15c54e98b112a73f0f05f2bbcd43c2b41d0e6
christian.moreira <christian.moreira@tre-rj.jus.br>
2025-09-26 08:29:43 -0300

    lista

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -7,6 +7,8 @@ import { PgAreaGestorComponent } from './modules/area-logada/pg-area-gestor/pg-a
 import { PgAreaAdvogadoComponent } from './modules/area-logada/pg-area-advogado/pg-area-advogado.component';
 import { PgEsqueceuSenhaComponent } from './modules/autenticacao/pg-esqueceu-senha/pg-esqueceu-senha.component';
 import { PgTrocaSenhaComponent } from './modules/autenticacao/pg-troca-senha/pg-troca-senha.component';
+import { PgListaTrataProcessoComponent } from './modules/sustentacao/pg-lista-trata-processo/pg-lista-trata-processo.component';
+import { PgSolicitacaoSustentacaoComponent } from './modules/sustentacao/pg-solicitacao-sustentacao/pg-solicitacao-sustentacao.component';
 
 export const routes: Routes = [
   {
@@ -42,6 +44,16 @@ export const routes: Routes = [
   {
     path: 'areaAdvogado',
     component: PgAreaAdvogadoComponent,
+    canActivate: [autenticadoGuard]
+  },
+  {
+    path: 'lista-trata-processo',
+    component: PgListaTrataProcessoComponent,
+    canActivate: [autenticadoGuard]
+  },
+  {
+    path: 'solicitacao-sustentacao/:processoId',
+    component: PgSolicitacaoSustentacaoComponent,
     canActivate: [autenticadoGuard]
   },
 
